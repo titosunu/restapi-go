e1d8426ecbb9047e82aa92de5465f3df34c70d32
titosunu <titosunu2001@gmail.com>
2024-07-29 15:55:46 +0700

    dependency injection

---
+++ test/simple_service_test.go
@@ -0,0 +1,12 @@
+package test
+
+import (
+	"fmt"
+	"rest-api/simple"
+	"testing"
+)
+
+func TestSimpleService(t *testing.T) {
+	simpleService := simple.InitializedService()
+	fmt.Println(simpleService.SimpleRepository)
+}
