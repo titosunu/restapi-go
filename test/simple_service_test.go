77ac681546f60e3285819194fada9dfdae34ace2
titosunu <titosunu2001@gmail.com>
2024-07-29 18:15:56 +0700

    dependency injection

--- test/simple_service_test.go
+++ test/simple_service_test.go
@@ -4,9 +4,24 @@ import (
 	"fmt"
 	"rest-api/simple"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSimpleService(t *testing.T) {
-	simpleService := simple.InitializedService()
+	simpleService, err := simple.InitializedService(true)
+	fmt.Println(err)
 	fmt.Println(simpleService.SimpleRepository)
+}
+
+func TestSimpleServiceError(t *testing.T) {
+	simpleService, err := simple.InitializedService(true)
+	assert.NotNil(t, err)
+	assert.Nil(t, simpleService)
+}
+
+func TestSimpleServiceSuccess(t *testing.T) {
+	simpleService, err := simple.InitializedService(false)
+	assert.Nil(t, err)
+	assert.NotNil(t, simpleService)
 }
